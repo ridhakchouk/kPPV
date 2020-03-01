894177e24152162b2512d44795af6f1ba313ba05
Kchouk Ridha <ridha.kchouk@gmail.com>
2020-03-02 00:08:29 +0100

    Mise à jour du code plus reponse a la question 5

--- src/kPPV.java
+++ src/kPPV.java
@@ -1,7 +1,5 @@
 import java.io.*;
 import java.util.Arrays;
-import java.util.Scanner;
-import java.util.Collection;
 
 /**
  * @author hubert.cardot
@@ -27,36 +25,108 @@ public class kPPV {
 		// distances: table to store all distances between the given exemple X and all
 		// exemples in learning set, using ComputeDistances
 		Double distances[] = new Double[NbClasses * NbExLearning];
+		Double X[] = { 5.4, 3.7, 1.5, 0.2 };
+		distances = ComputeDistances(X, distances);
 
-		Double X[] = { 5.5, 2.3, 4.0, 1.3 };
+//		System.out.println("On calcul la distance par rapport aux données suivantes : { " + X[0] + "; " + X[1] + "; "
+//				+ X[2] + "; " + X[3] + " }");
+//		System.out.println("Liste des distances :");
+//		for (int i = 0; i < distances.length; i++) {
+//			System.out.println("Distance n° " + (i + 1) + " = " + distances[i]);
+//		}
 
-		// To be done
-		Double ma_liste[] = ComputeDistances(X, distances);
+//		System.out.println("classe d'appartenance : " + one_ppv(X, distances));
+//		k_ppv(distances,3);
+//		preparation du K-PPV
 
-		for (int i = 0; i < ma_liste.length; i++) {
-			System.out.println("Distance n° " + i + " = " + ma_liste[i]);
-		}
-
-		System.out.println(one_ppv(X, distances));
-
-		// preparation du K-PPV
-		System.out.println("Entrer le paramètre K, pour K plus proche voisins ");
-		Scanner sc = new Scanner(System.in);
-		int k = sc.nextInt();
-		sc.close();
-
-		Arrays.sort(ma_liste);
-		Double[] kPPV = new Double[k];
-
-		System.out.println("Disntaces des voisins les plus proches : ");
-		for (int i = 0; i < k; i++) {
-			kPPV[i] = ma_liste[i];
-			System.out.println("Voisin n°"+i+" "+ma_liste[i]);
-		}
+		// confusion(data, distances);
+		knnconfusion(data,0);
 
 	}
 
-	// OK
+	public static void knnconfusion(Double data[][][],int knn) {
+		
+		Double tab[] = new Double[4];
+		for (int i = 0; i < NbClasses; i++) {
+
+			for (int j = 0; j < NbEx; j++) {
+
+				for(int k = 0 ; k<NbFeatures;k++) {
+//					System.out.print(data[i][j][k]+"\t");
+					tab[k]=data[i][j][k];
+				}
+				
+				System.out.println();
+			}
+			System.out.println();
+
+		}
+	}
+
+	public static int[][] confusion(Double data[][][], Double distances[]) {
+		int conf_mat[][] = new int[3][3];
+		Double x[] = new Double[4];
+
+		for (int i = 0; i < NbClasses; i++) {
+			for (int j = NbExLearning; j < NbEx; j++) {
+				for (int k = 0; k < NbFeatures; k++) {
+					x[k] = data[i][j][k];
+				}
+				int classe = one_ppv(x, distances);
+				// System.out.println(i);
+				conf_mat[i][classe] += 1;
+			}
+		}
+
+		Double somme = 0.0;
+		int item1 = 0;
+		int item2 = 0;
+
+		System.out.println("Confusion KPPV : ");
+		System.out.println();
+		System.out.println("\t0\t1\t2");
+		System.out.print("\t-------------------");
+		for (int i = 0; i < conf_mat.length; i++) {
+			System.out.println();
+			System.out.print(i + " |");
+			for (int j = 0; j < conf_mat[i].length; j++) {
+				System.out.print("\t" + conf_mat[i][j] + " ");
+
+				item1 += conf_mat[i][j];
+
+				if (i == j) {
+
+					item2 += conf_mat[i][j];
+				}
+			}
+		}
+
+		somme = (double) (item2 * 100.0) / (item1);
+		System.out.println();
+		System.out.println();
+		System.out.println("Rate : " + somme + "%");
+		System.out.println("Error : " + (100.0 - somme) + "%");
+		return conf_mat;
+	}
+
+	// This function returnes a list of K NN where k is ask to be entered by user
+	// question 5
+	public static Double[] k_ppv(Double[] tab, int k) {
+
+		Double[] res = new Double[k];
+		Arrays.sort(tab);
+
+		System.out.println("Distances des K voisins les plus proches : ");
+		for (int i = 0; i < k; i++) {
+			res[i] = tab[i];
+			System.out.println("Voisin n°" + (i + 1) + " " + res[i]);
+		}
+
+		return res;
+	}
+
+	// This method return a list of distance according to an array which is
+	// containing coordinates
 	private static Double[] ComputeDistances(Double x[], Double distances[]) {
 		// ---compute the distance between an input data x to test and all examples in
 		// training set (in data)
@@ -75,6 +145,7 @@ public class kPPV {
 		return distances;
 	}
 
+	// This method return the class of X
 	private static int one_ppv(Double x[], Double distances[]) {
 		int indexClass = 0;
 		Double res[] = ComputeDistances(x, distances);
@@ -86,16 +157,16 @@ public class kPPV {
 			}
 		}
 		if (indexClass < 25) {
-			return 1;
+			return 0;
 		}
 		if (indexClass > 24 && indexClass < 50) {
-			return 2;
+			return 1;
 		}
 		if (indexClass > 49) {
-			return 3;
+			return 2;
 		}
 
-		return 0;
+		return -1;
 
 	}
 
