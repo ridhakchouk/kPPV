5716a5d8c6b999dc472c07e91ba458ab512dc94e
Kchouk Ridha <ridha.kchouk@gmail.com>
2020-03-07 22:50:53 +0100

    Génération d'une nouvelle documentation, mise à jour des réponses, optimisation du code. Il reste à améliorer le one ppv pour le fiabiliser afin de déterminer correctement les classes en fonction des distances

--- src/kPPV.java
+++ src/kPPV.java
@@ -1,18 +1,20 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
- *@author ridha.kchouk
+ * @author ridha.kchouk
  * 
  */
 public class kPPV {
 	/**
-	 *  Variables générales pour le traitement des données Iris (référentiel UCI)
-	 * NbEx : nombre de données par classe dans l'ensemble de données
-	 * NbClasses : Nombre de classes à reconnaître
-	 * NbFeatures : Dimensionnalité de l'ensemble de données
-	 * NbExLearning : Nombre d'exemples par classe utilisés pour l'apprentissage (il y a les
-	 * les premiers dans le stockage des données pour chaque classe)
+	 * Variables générales pour le traitement des données Iris (référentiel UCI)
+	 * NbEx : nombre de données par classe dans l'ensemble de données NbClasses :
+	 * Nombre de classes à reconnaître NbFeatures : Dimensionnalité de l'ensemble de
+	 * données NbExLearning : Nombre d'exemples par classe utilisés pour
+	 * l'apprentissage (il y a les les premiers dans le stockage des données pour
+	 * chaque classe)
 	 * 
 	 */
 	static int NbEx = 50, NbClasses = 3, NbFeatures = 4, NbExLearning = 25;
@@ -22,69 +24,59 @@ public class kPPV {
 	public static void main(String[] args) {
 		System.out.println("Starting kPPV");
 		ReadFile();
-
 		/**
-		* 
-		* X est un exemple à classer (à prendre en données - exemples de tests-)
-		* Double X[] = nouveau Double [NbFeatures] ;
-		* distances : tableau permettant de stocker toutes les distances entre l'exemple donné X et toutes
-		* exemples dans la série d'apprentissage, en utilisant ComputeDistances
-		*/
+		 * 
+		 * X est un exemple à classer (à prendre en données - exemples de tests-) Double
+		 * X[] = nouveau Double [NbFeatures] ; distances : tableau permettant de stocker
+		 * toutes les distances entre l'exemple donné X et toutes exemples dans la série
+		 * d'apprentissage, en utilisant ComputeDistances
+		 */
 		Double distances[] = new Double[NbClasses * NbExLearning];
-		Double X[] = { 5.4, 3.7, 1.5, 0.2 };
-		distances = ComputeDistances(X, distances);
+		Double X[] = { 4.8,3.4,1.9,0.2 };
 
 		System.out.println("On calcul la distance par rapport aux données suivantes : { " + X[0] + "; " + X[1] + "; "
 				+ X[2] + "; " + X[3] + " }");
-		System.out.println("Liste des distances :");
-		for (int i = 0; i < distances.length; i++) {
-			System.out.println("Distance n° " + (i + 1) + " = " + distances[i]);
-		}
+		System.out.println("Calcul des distances...");
+		distances = ComputeDistances(X, distances);
+		String rep = "";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Voir les distances ? (Oui/Non)");
+		while (!rep.equals("Oui") || !rep.equals("Non")) {
+			rep = sc.next();
 
-		System.out.println("classe d'appartenance : " + one_ppv(X, distances));
-		k_ppv(distances,3);
-
-		confusion(data, distances);
-		//knnconfusion(data,0);
-
-	}
-
-	/**
-	 * 
-	 * @implNote en cours de dev
-	 * 
-	 * */
-	public static void knnconfusion(Double data[][][],int knn) {
-		
-		Double tab[] = new Double[4];
-		for (int i = 0; i < NbClasses; i++) {
-
-			for (int j = 0; j < NbEx; j++) {
-
-				for(int k = 0 ; k<NbFeatures;k++) {
-//					System.out.print(data[i][j][k]+"\t");
-					tab[k]=data[i][j][k];
+			if (rep.equals("Oui")) {
+				System.out.println("Liste des distances :");
+				for (int i = 0; i < distances.length; i++) {
+					System.out.println("Distance n° " + (i + 1) + " = " + distances[i]);
 				}
-				
+				break;
+			} else if (rep.equals("Non")) {
+				break;
 			}
 
-
 		}
+
+		
+		System.out.println("Entrée le paramètre K pour les K plus proches voisins : ");
+		int k = sc.nextInt();
+		kppv(X, distances, k);
+		sc.close();
+
+		confusion(data, distances);
+
 	}
 
-	
 	/**
 	 * Question 4
 	 * 
-	 * @param data[][][]
-	 * 		tableau à trois dimension contenant respectivement [nombre_de_classe][nombre_element_par_classe][nombre_de_caracteristique]
+	 * @param data[][][]  tableau à trois dimension contenant respectivement
+	 *                    [nombre_de_classe][nombre_element_par_classe][nombre_de_caracteristique]
 	 * 
-	 * @param distances[]
-	 * 		tableau contenant des distances de type Double
+	 * @param distances[] tableau contenant des distances de type Double
 	 * 
 	 * @return retourne une matrice de confusion
 	 * 
-	 * */
+	 */
 	public static int[][] confusion(Double data[][][], Double distances[]) {
 		int conf_mat[][] = new int[3][3];
 		Double x[] = new Double[4];
@@ -104,7 +96,7 @@ public class kPPV {
 		int item1 = 0;
 		int item2 = 0;
 
-		System.out.println("Confusion KPPV : ");
+		System.out.println("Confusion : ");
 		System.out.println();
 		System.out.println("\t0\t1\t2");
 		System.out.print("\t-------------------");
@@ -131,47 +123,51 @@ public class kPPV {
 		return conf_mat;
 	}
 
-
 	/**
 	 * Question 5 ..
-	 *@param x[]
-	 *		tableau contenant 4 éléments de type Double.
-	 *		il s'agit des coordonnées.
-	 *
-	 *@param k
-	 *		permet de déterminer combien de voisin la fonction doit
-	 *		retourner
 	 * 
-	 * @return retourne une liste de distance des k voisins plus proche
+	 * @param x[] 			tableau contenant 4 éléments de type Double. il s'agit des
+	 *           			coordonnées.
+	 *
+	 * @param distances[] 	Tableau contenant plusieurs éléments de type Double. il
+	 *                 	  	s'agit d'une liste de distance par rapport au paramètre
+	 *                  	x[].
+	 *
+	 *
+	 * @param k  			permet de déterminer combien de voisin la fonction doit retourner
+	 * 
+	 * 
+	 * @return retourne la classe majoritaire (prédite)
 	 */
-	
-	public static Double[] k_ppv(Double x[], int k) {
 
-		Double[] res = new Double[k];
-		Arrays.sort(x);
+	public static int kppv(Double x[], Double distances[], int k) {
 
-		System.out.println("Distances des K voisins les plus proches : ");
+		Arrays.sort(distances);
+		Double k_dist_proche[] = new Double[k];
+		System.out.println("Les " + k + " voisins les plus proches sont à une distance de :");
 		for (int i = 0; i < k; i++) {
-			res[i] = x[i];
-			System.out.println("Voisin n°" + (i + 1) + " " + res[i]);
+			k_dist_proche[i] = distances[i];
+			System.out.println("Voisin plus proche n°"+(i+1)+" :"+distances[i]);
 		}
+		
+		
+		System.out.println("Classe la plus fréquente : "+one_ppv_v2(x, k_dist_proche));
+		return one_ppv_v2(x, k_dist_proche);
 
-		return res;
 	}
 
-	
 	/**
 	 * Question 2 :
-	 *@param x[]
-	 *		tableau contenant 4 éléments de type Double.
-	 *		il s'agit des coordonnées.
+	 * 
+	 * @param x[]         tableau contenant 4 éléments de type Double. il s'agit des
+	 *                    coordonnées.
 	 *
-	 *@param distances[]
-	 *		initialement tableau vide de Type double qui sera rempli
-	 *		au fur et à mesure en fonction du paramètre x[].
-	 *		Contiendra une liste de distance entre le param x[] et les autres points
-	 *		
-	 *@return retourne une liste de distance
+	 * @param distances[] initialement tableau vide de Type double qui sera rempli
+	 *                    au fur et à mesure en fonction du paramètre x[].
+	 *                    Contiendra une liste de distance entre le param x[] et les
+	 *                    autres points
+	 * 
+	 * @return retourne une liste de distance
 	 */
 	private static Double[] ComputeDistances(Double x[], Double distances[]) {
 		int t = 0;
@@ -190,34 +186,89 @@ public class kPPV {
 
 	/**
 	 * Question 3 :
-	 *@param x[]
-	 *		tableau contenant 4 éléments de type Double.
-	 *		il s'agit des coordonnées.
+	 * 
+	 * @param x[]         tableau contenant 4 éléments de type Double. il s'agit des
+	 *                    coordonnées.
 	 *
-	 *@param distances[]
-	 *		Tableau contenant plusieurs éléments de type Double.
-	 *		il s'agit d'une liste de distance par rapport au paramètre x[].
+	 * @param distances[] Tableau contenant plusieurs éléments de type Double. il
+	 *                    s'agit d'une liste de distance par rapport au paramètre
+	 *                    x[].
 	 *
-	 *@return retourne la classe de x[].
+	 * @return retourne la classe de x[].
 	 *
 	 **/
 	private static int one_ppv(Double x[], Double distances[]) {
-		int indexClass = -1;
 		Double res[] = ComputeDistances(x, distances);
 		Double resultat = res[0];
+		int indexClass = 99999;
 		for (int i = 0; i < res.length; i++) {
 			if (res[i] < resultat) {
 				resultat = res[i];
 				indexClass = i;
+
 			}
 		}
-		if (indexClass < 25) {
+		if (indexClass < NbExLearning) {
 			return 0;
 		}
-		if (indexClass > 24 && indexClass < 50) {
+		if (indexClass > NbExLearning - 1 && indexClass < NbExLearning * 2) {
 			return 1;
 		}
-		if (indexClass > 49) {
+		if (indexClass > (NbExLearning * 2) - 1) {
+			return 2;
+		}
+
+		// Retourne -1 en cas d'erreur
+		return -1;
+
+	}
+	
+	
+	private static int one_ppv_v2(Double x[], Double distances[]) {
+		Double res[] =distances;
+		Double resultat = res[0];
+		int indexClass = 99999;
+		for (int i = 0; i < res.length; i++) {
+			if (res[i] < resultat) {
+				resultat = res[i];
+				indexClass = i;
+
+			}
+		}
+		if (indexClass < NbExLearning) {
+			return 0;
+		}
+		if (indexClass > NbExLearning - 1 && indexClass < NbExLearning * 2) {
+			return 1;
+		}
+		if (indexClass > (NbExLearning * 2) - 1) {
+			return 2;
+		}
+
+		// Retourne -1 en cas d'erreur
+		return -1;
+
+	}
+	
+	
+	private static int one_ppv_v3(Double x[], Double distances[]) {
+		Double res[] =distances;
+		Double resultat = res[0];
+		int indexClass = 99999;
+		for (int i = 0; i < res.length; i++) {
+			if (res[i] < resultat) {
+				resultat = res[i];
+				indexClass = i;
+
+			}
+		}
+		if (indexClass < NbExLearning) {
+			return 0;
+		}
+		if (indexClass > NbExLearning - 1 && indexClass < NbExLearning * 2) {
+			return 1;
+		}
+		if (indexClass > (NbExLearning * 2) - 1) {
 			return 2;
 		}
 
@@ -226,10 +277,9 @@ public class kPPV {
 
 	}
 
-	
 	/**
-	 *cette fonction permet la lecture du fichier de données
-	 *et remplit la variable "data" de ces données
+	 * cette fonction permet la lecture du fichier de données et remplit la variable
+	 * "data" de ces données
 	 * 
 	 **/
 	private static void ReadFile() {
